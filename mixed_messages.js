9c64da8820e8e5b34ccf0802c23619d56629685a
Trevor Harbeard <trevor.harbeard@proton.me>
2023-07-23 10:29:43 -0700

    Added message creation

--- mixed_messages.js
+++ mixed_messages.js
@@ -8,7 +8,33 @@ const poemComps = {
     domain: ['the void', 'the heavens', 'the aethyr', 'the darkness'],
     request: ['a sign', 'a token', 'a boon', 'a reason'],
     envoy: ['great angel', 'squamous entity', 'writhing tentacle', 'hellish visage'],
-    response: ['Which way to the restroom?', 'Are you my dinner?', 'You come here often?', 'Did your mother dress you?']
+    response: ['"Which way to the restroom?"', '"Are you my dinner?"', '"You come here often?"', '"Did your mother dress you?"']
+};
+
+// create the message
+let message = []
+
+// add components
+for (let comp in poemComps) {
+    idx = randIdxValue(poemComps[comp].length);
+
+    switch (comp) {
+        case 'domain': 
+            message.push(`I called into ${poemComps[comp][idx]}`);
+            break;
+        case 'request':
+            message.push(`To give me ${poemComps[comp][idx]};`);
+            break;
+        case 'envoy':
+            message.push(`A ${poemComps[comp][idx]} appeared,`);
+            break;
+        case 'response':
+            message.push(`${poemComps[comp][idx]} it asked.`);
+            break;
+        default:
+            message.push('I am alone.');
+            break;
+    }
 }
 
-
+console.log(message);
