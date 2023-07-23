f971e107f1d0b81827e2d9883e3eb211e3b03c1c
Trevor Harbeard <trevor.harbeard@proton.me>
2023-07-23 09:58:41 -0700

    Added random index generator and components collection

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,1 +1,14 @@
-console.log("Testing...testing...")
+// utility to obtain a random index value, where num is the length of the array
+function randIdxValue(num) {
+    return Math.floor(Math.random() * num);
+}
+
+// a collection of components used to build the poem
+const poemComps = {
+    domain: ['the void', 'the heavens', 'the aethyr', 'the darkness'],
+    request: ['a sign', 'a token', 'a boon', 'a reason'],
+    envoy: ['great angel', 'squamous entity', 'writhing tentacle', 'hellish visage'],
+    response: ['Which way to the restroom?', 'Are you my dinner?', 'You come here often?', 'Did your mother dress you?']
+}
+
+
